c46a1a7abf1c4eb0fc5f1f72611e56ace9253747
Gherex <germilagger@gmail.com>
2025-05-27 01:41:27 -0300

    change base url

--- src/api/fetchNotes.js
+++ src/api/fetchNotes.js
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080/notes";
+// const BASE_URL = "http://localhost:8080/api/v1/notes";
+const BASE_URL = "https://noteapp-api-4eyd.onrender.com/api/v1/notes";
 
 export const fetchAllNotes = () =>
   fetch(`${BASE_URL}`).then((res) => res.json());
